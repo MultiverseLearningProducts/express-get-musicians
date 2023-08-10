134738e799d143f1ff09993d7bccb757f17a2b57
Kehinde. A <whenkehindewalks@gmail.com>
2023-08-10 02:04:21 -0400

    musician route & Band Route updated

--- index.test.js
+++ index.test.js
@@ -5,7 +5,7 @@ execSync("npm run seed");
 
 const request = require("supertest"); //The response to an endpoint can be accessed using the supertest package.
 const { db } = require("./db/connection");
-const { Musician } = require("./models/index");
+const { Musician, Band } = require("./models/index");
 const app = require("./src/app");
 const seedMusician = require("./seedData");
 
@@ -14,7 +14,17 @@ describe("./musicians endpoint", () => {
   it("Testing musician endpoint", async () => {
     const res = await request(app).get("/musicians");
     const responseData = JSON.parse(res.text);
-    console.log(responseData);
+    // console.log(responseData);
+    expect(res.statusCode).toBe(200);
+  });
+});
+
+describe("./bands endpoint", () => {
+  // Write your tests here
+  it("Testing band endpoint", async () => {
+    const res = await request(app).get("/bands");
+    const responseData = JSON.parse(res.text);
+    // console.log(responseData);
     expect(res.statusCode).toBe(200);
   });
 });
--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const { Musician } = require("../models/index");
+const { Musician, Band } = require("../models/index");
 const { db } = require("../db/connection");
 
 const port = 3000;
@@ -9,6 +9,25 @@ const port = 3000;
 app.get("/musicians", async (req, res) => {
   const musicians = await Musician.findAll();
   res.json(musicians);
+});
+
+app.get("/musicians/1", async (req, res) => {
+  const musician = await Musician.findByPk(1);
+  res.json(musician);
+});
+app.get("/musicians/2", async (req, res) => {
+  const musician = await Musician.findByPk(2);
+  res.json(musician);
+});
+app.get("/musicians/3", async (req, res) => {
+  const musician = await Musician.findByPk(3);
+  res.json(musician);
+});
+
+//TODO: Create a GET /bands route to return all bandss
+app.get("/bands", async (req, res) => {
+  const bands = await Band.findAll();
+  res.json(bands);
 });
 
 module.exports = app;
