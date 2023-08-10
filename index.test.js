7c54b97c96d3337f3dcca22700c498fa8a936dae
Kehinde. A <whenkehindewalks@gmail.com>
2023-08-10 01:51:21 -0400

    Index Test Updated

--- index.test.js
+++ index.test.js
@@ -1,22 +1,20 @@
 // install dependencies
-const { execSync } = require('child_process');
-execSync('npm install');
-execSync('npm run seed');
+const { execSync } = require("child_process");
+execSync("npm install");
+execSync("npm run seed");
 
-const request = require("supertest")
-const { db } = require('./db/connection');
-const { Musician } = require('./models/index')
-const app = require('./src/app');
+const request = require("supertest"); //The response to an endpoint can be accessed using the supertest package.
+const { db } = require("./db/connection");
+const { Musician } = require("./models/index");
+const app = require("./src/app");
 const seedMusician = require("./seedData");
 
-
-describe('./musicians endpoint', () => {
-    // Write your tests here
-    
-    
-
-
-
-
-    
-})
+describe("./musicians endpoint", () => {
+  // Write your tests here
+  it("Testing musician endpoint", async () => {
+    const res = await request(app).get("/musicians");
+    const responseData = JSON.parse(res.text);
+    console.log(responseData);
+    expect(res.statusCode).toBe(200);
+  });
+});
