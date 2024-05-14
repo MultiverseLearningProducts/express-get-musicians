dbf1aed8453b763dad36e40086173835e2e58516
Andrii-Mysheniuk <amysheniuk@webfx.co.uk>
2024-05-14 12:10:54 +0100

    add task solution

--- src/app.js
+++ src/app.js
@@ -1,16 +1,30 @@
 const express = require("express");
 const app = express();
-const { Musician } = require("../models/index")
+const { Musician, Band } = require("../models/index")
 const { db } = require("../db/connection")
 
 const port = 3000;
 
 //TODO: Create a GET /musicians route to return all musicians 
+app.get('/musicians', async (req, res) => {
+  const musicians = await Musician.findAll()
+  res.json(musicians)
+})
 
+app.get('/musicians/:id', async (req, res) => {
+  const musicianId = parseInt(req.params.id)
+  const musicians = await Musician.findAll()
 
+  if(musicianId >= 1 && musicianId <= 3) {
+    res.json(musicians[musicianId - 1])
+  } else {
+    res.status(404).json({ message: 'Musician not found' })
+  }
+})
 
-
-
-
+app.get('/bands', async (req, res) => {
+  const bands = await Band.findAll()
+  res.json(bands)
+})
 
 module.exports = app;
