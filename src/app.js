ef7900686668b13a0b790c08b5af6d8ebce215b8
Andrii-Mysheniuk <amysheniuk@webfx.co.uk>
2024-05-14 17:09:45 +0100

    Express Musicians Part 2,3 without tests

--- src/app.js
+++ src/app.js
@@ -6,8 +6,17 @@ const { db } = require("../db/connection")
 const port = 3000;
 
 //TODO: Create a GET /musicians route to return all musicians 
+app.use(express.json())
+app.use(express.urlencoded())
+
 app.get('/musicians', async (req, res) => {
   const musicians = await Musician.findAll()
+  res.json(musicians)
+})
+
+app.post('/musicians', async (req, res) => {
+  const musicians = await Musician.findAll()
+  musicians.push(req.body)
   res.json(musicians)
 })
 
@@ -20,6 +29,22 @@ app.get('/musicians/:id', async (req, res) => {
   } else {
     res.status(404).json({ message: 'Musician not found' })
   }
+})
+
+app.put('/musicians/:id', async (req, res) => {
+  const musicians = await Musician.findAll()
+  const id = req.params.id
+  musicians[id - 1] = req.body
+
+  res.json(musicians)
+})
+
+app.delete('/musicians/:id', async (req, res) => {
+  const musicians = await Musician.findAll()
+  const id = req.params.id
+  musicians.splice(id - 1, 1)
+
+  res.json(musicians)
 })
 
 app.get('/bands', async (req, res) => {
